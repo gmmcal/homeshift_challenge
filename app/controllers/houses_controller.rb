524f88c4eab074013fdd649a0c44bdd0dd0427c2
Gustavo Cunha <gustavocunha@gmail.com>
2015-08-01 11:43:02 -0300

    Add create a house form Add simpleform gem

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -2,4 +2,23 @@ class HousesController < ApplicationController
   def index
     @houses = House.all
   end
+
+  def new
+    @house = House.new
+  end
+
+  def create
+    @house = House.new(house_params)
+
+    if @house.save
+      redirect_to houses_path, notice: 'House was successfuly created.'
+    else
+      render :new
+    end
+  end
+
+  private
+    def house_params
+      params.require(:house).permit(:title, :description, :address, :postcode)
+    end
 end
